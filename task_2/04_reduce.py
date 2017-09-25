54e188b9329bfbbefc2d5b5bbd6a428e4c15ea80
ddonetskov <ddonetskov@gmail.com>
2017-09-25 16:37:29 +0300

    Created the map and the reduce scripts

---
+++ task_2/04_reduce.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3.6
+
+import datetime
+# import io
+import pyarrow as pa
+import re
+import sys
+import task_2_mod as g
+
+################################################################################
+# Initialization
+################################################################################
+
+if g.DEST_FS_TYPE == 'FS':
+    pass
+elif g.DEST_FS_TYPE == 'HDFS':
+    hdfs = pa.hdfs.connect()
+else:
+    raise Exception("Unknown file system.")
+
+################################################################################
+# Main
+################################################################################
+
+# going through the lines of key, values
+
+prev_key = None
+out_file = None
+
+for line in sys.stdin:
+
+    key, value = line.split('\t')
+
+    # print('Processing ' + key)
+
+    if key != prev_key:
+        if out_file != None:
+            out_file.close();
+        # create the new file according to the date
+        key_date = datetime.datetime.strptime(key, '"%Y-%m-%d"')
+        out_file_name = g.ARCHIVE_DIR + '/' + str(key_date.year) + '/' + str(key_date.month) + '/' + str(key_date.day) + '/data.csv'
+        out_file = hdfs.open(out_file_name, 'wb')
+        print('Created the file ' + out_file_name)
+     
+    out_file.write(value)
+
+    prev_key = key
+
+if out_file != None:
+    out_file.close();
---
+++ task_2/task_2_mod.py
@@ -0,0 +1,14 @@
+################################################################################
+# Global
+################################################################################
+DEST_FS_TYPE = 'HDFS'
+
+if DEST_FS_TYPE == 'FS':
+    raise Exception("Unsupported.")
+elif DEST_FS_TYPE == 'HDFS':
+    INCOMING_DIR = '/mts/task_2/incoming'
+    STAGING_DIR  = '/mts/task_2/staging'
+    ARCHIVE_DIR  = '/mts/task_2/archive'
+else:
+    raise Exception("Unknown file system.")
+
