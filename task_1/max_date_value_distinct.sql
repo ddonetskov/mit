def78b5f040254780068e7908e8e6b025c7e0e11
ddonetskov <ddonetskov@gmail.com>
2017-09-27 10:12:16 +0300

    Added steps to create the test table.

--- task_1/max_date_value_distinct.sql
+++ task_1/max_date_value_distinct.sql
@@ -1,20 +1,45 @@
--- Version 1
+drop table task_1;
+
+create table task_1 (
+  id         number,
+  value      number,
+  vdate      date);
+
+alter session set nls_date_format = 'YYYY-MM-DD';
+
+insert into task_1 values (1, 5, '2017-01-01');
+insert into task_1 values (1, 6, '2017-01-01');
+insert into task_1 values (2, 2, '2017-01-02');
+insert into task_1 values (3, 5, '2017-05-06');
+insert into task_1 values (3, 6, '2017-06-06');
+
+-- TBC generator
+begin
+  for i in 1..200 loop
+    null;
+  end loop;
+end;
+/
+
+commit;
+
+select * from task_1;
+
+-- The row with max date and max value, version 1
 select t.*
-  from (select rownum, id, date, value
-          from tab
-	     where date = (select max(date) from tab)
+  from (select rownum, id, vdate, value
+          from task_1
+         where vdate = (select max(vdate) from task_1)
          order by value desc) t
- where t.rownum = 1;
+ where rownum = 1;
 
--- Version 2
+-- The row with max date and max value, version 2
 with mdv as
-  (select id, date, value
-     from tab
-	where date = (select max(date) from tab)
+  (select id, vdate, value
+     from task_1
+    where vdate = (select max(vdate) from task_1)
     order by value desc)
-select distinct id, value, date
-  from tab, 
-       mdv
- where (id, date, value) in (select id, date, value from tab where rownum = 1);
+select distinct id, value, vdate
+  from task_1 t1
+ where (t1.id, t1.vdate, t1.value) in (select id, vdate, value from mdv where rownum = 1);
 
- 
